d9da387a2dcc161f3ef0a924d157859a33ec77ce
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-04-22 21:44:02 -0300

    Finished the challenge

---
+++ README.md
@@ -0,0 +1,33 @@
+<img alt="GoStack" src="/assets/desafio.png" />
+
+<h3 align="center">
+  Desafio 06: Banco de dados e upload de arquivos no Node.js
+</h3>
+
+## :rocket: Sobre o desafio
+
+Nesse desafio, foi continuaado o desenvolvimento da aplicação de gestão de transações, treinando o que foi aprendido até agora no Node.js junto ao TypeScript, mas dessa vez incluindo o uso de banco de dados com o TypeORM e envio de arquivos com o Multer!
+
+Essa é uma aplicação que armazena transações financeiras de entrada e saída e permiti o cadastro e a listagem dessas transações, além de permitir a criação de novos registros no banco de dados a partir do envio de um arquivo csv.
+
+## Ferramentas utilizadas
+
+**Node.js**
+
+**TypeScript**
+
+## Como clonar e iniciar o projeto
+
+1. Clone o projeto utilizando o seguinte comando: ```git clone https://github.com/lucas-hgs/desafio-database-upload.git```
+
+2. Após clonar, basta rodar um ```yarn``` para instalar as dependências.
+
+3. Com o comando ```yarn dev:server``` você consegue rodar em modo de desenvolvimento.
+
+4. Para testar as funcionalidades, basta rodar ```yarn test```
+
+5. Para buildar o projeto, basta rodar ```yarn build```
+
+---
+
+Feito com 💜 por [Lucas](https://www.linkedin.com/in/lucas-hgs/) :wave:
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryRepository = getRepository(Category);
+
+    const enoughBalance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > enoughBalance.total) {
+      throw new AppError('Your balance is not enough', 400);
+    }
+
+    let checkCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategory) {
+      checkCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(checkCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: checkCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
